097df0b08016e72baeeb8c2934f3f35f5f6fec48
RafiTahmid <rafi.tahmid019@gmail.com>
2022-12-11 22:09:39 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Home from "./components/Home/Home";
+import Nav from "./components/Nav/Nav";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <Home></Home>
+    </div>
+  );
 }
 
 export default App;
