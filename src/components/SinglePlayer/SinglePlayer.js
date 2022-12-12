0ecca668c9b9a0ca467512957ff3336ac3bb1166
RafiTahmid <rafi.tahmid019@gmail.com>
2022-12-12 11:54:55 +0600

    fetched data using hooks

---
+++ src/components/SinglePlayer/SinglePlayer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SinglePlayer.css";
+
+const SinglePlayer = () => {
+  return (
+    <div>
+      <h2>From Single Player</h2>
+    </div>
+  );
+};
+
+export default SinglePlayer;
